5eb1b16eb7ff3754ecc9627c0993b13e3f8b79af
IRTJULIET <30794165+IRTJULIET@users.noreply.github.com>
2020-11-27 11:47:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,13 +62,13 @@ try:
     #LOADING FILE.
         @st.cache(persist = True)
         def sample_load_data():
-            sample_data = pd.read_csv('sampleDiabetes.csv')
+            sample_data = pd.read_csv('dc_test.csv')
             return sample_data
 
         sample_df = sample_load_data()
 
         def test_data():
-            tdata = pd.read_csv('sampleDiabetes.csv')
+            tdata = pd.read_csv('dc_test.csv')
             return tdata
 
         def load_data():
